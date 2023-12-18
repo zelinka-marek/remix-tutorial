7996f01217d32490e8bbef080646a4aaf35d09fb
Marek Zelinka <mzelinka17@gmail.com>
2023-12-18 14:32:27 +0100

    add styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { type LinksFunction } from "@remix-run/node";
 import {
   Form,
   Links,
@@ -6,6 +7,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import appStylesHref from "./index.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
 
 export default function App() {
   return (
